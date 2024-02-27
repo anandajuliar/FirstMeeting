b61023922a453ac791cbac3082cf1d06e7dae9e0
anandajuliar <anandajulia001@gmail.com>
2024-02-27 18:50:10 +0700

    Add project files.

---
+++ FirstMeeting/FirstMeeting.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+    float nJejari, nLuas; 
+    //numeric nJejari, nLuas
+    cout << "Masukan Jejari = "; 
+    //display 'Masukan Jejari =' 
+    cin >> nJejari; 
+    // accept nJejari
+    nLuas = 3.14 * nJejari * nJejari;
+        //compute nLuas = 3.14 * r * r
+        cout << "Luasnya =" << nLuas << endl; // display 
+}//end
